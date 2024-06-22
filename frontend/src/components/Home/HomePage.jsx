a3a98f919ab17f15b337ac7291b8fd335c83394f
DhanushRadhakrishna <dradh003@ucr.edu>
2024-06-22 14:49:39 -0700

    homepage changes

--- frontend/src/components/Home/HomePage.jsx
+++ frontend/src/components/Home/HomePage.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import styles from '../../css/HomePage.module.css';
-
+import { Button, Form, FormLabel, Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function Homepage() {
   const formik = useFormik({
     initialValues: {
@@ -20,38 +21,41 @@ function Homepage() {
 
   return (
     <div className={styles.Homepage}>
-      <header className={styles['Homepage-header']}>
-        <h1>Learning Planner</h1>
-        <form onSubmit={formik.handleSubmit}>
-          <div>
-            <label>
-              I want to learn about
-              <input
-                type="text"
-                name="topic"
-                onChange={formik.handleChange}
-                value={formik.values.topic}
-                placeholder="e.g., React, Python, etc."
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              in
-              <input
-                type="number"
-                name="hours"
-                onChange={formik.handleChange}
-                value={formik.values.hours}
-                placeholder="e.g., 10"
-              />
-              hrs
-            </label>
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      </header>
-    </div>
+    <header className={styles['Homepage-header']}>
+      <h1>Learning Planner</h1>
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md="6">
+            <Form onSubmit={formik.handleSubmit} className={styles['styled-form']}>
+              <Form.Group>
+                <Form.Control
+                  type="text"
+                  name="topic"
+                  onChange={formik.handleChange}
+                  value={formik.values.topic}
+                  placeholder="Enter something you wanna learn"
+                  // className={styles['full-width']}
+                />
+              </Form.Group>
+              <Form.Group className={styles['inline-form-group']}>
+                <FormLabel>In</FormLabel>
+                <Form.Control
+                  type="number"
+                  name="hours"
+                  onChange={formik.handleChange}
+                  value={formik.values.hours}
+                  placeholder="5"
+                  className={styles['full-width']}
+                />
+                <FormLabel>hours</FormLabel>
+              </Form.Group>
+              <Button type="submit">Submit</Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </header>
+  </div>
   );
 }
 
