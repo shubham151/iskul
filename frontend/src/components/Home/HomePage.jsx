4d61e3c14f714bf2690bfca76421346019b32712
Shubham Mishra <shubham.cmishra@gmail.com>
2024-06-22 17:35:50 -0700

    Corrected css

--- frontend/src/components/Home/HomePage.jsx
+++ frontend/src/components/Home/HomePage.jsx
@@ -31,33 +31,37 @@ function Homepage() {
     <div className={styles.Homepage + "text-center my-5"}>
       <header className={styles['Homepage-header']}>
         <h1>Learning Planner</h1>
-        <form onSubmit={formik.handleSubmit}>
-          <div>
-            <label>
-              I want to learn about
+        <form onSubmit={formik.handleSubmit} className='mt-4'>
+          <div className='mb-3'>
+            <label className='form-label'>
+              {/* I want to learn about */}
               <input
                 type="text"
                 name="topic"
                 onChange={formik.handleChange}
                 value={formik.values.topic}
-                placeholder="e.g., React, Python, etc."
+                placeholder="Enter something you want to learn"
+                className="form-control bg-dark text-white ms-2 me-2"
               />
             </label>
           </div>
-          <div>
-            <label>
-              in
-              <input
-                type="number"
-                name="hours"
-                onChange={formik.handleChange}
-                value={formik.values.hours}
-                placeholder="e.g., 10"
-              />
-              hrs
-            </label>
+          <div className='mb-3'>
+          <label className='form-label'>
+            in 
+            <input
+              type="number"
+              name="hours"
+              onChange={formik.handleChange}
+              value={formik.values.hours}
+              placeholder="e.g. 10"
+              className="form-control bg-dark text-white ms-2 me-2"
+              style={{ width: '70px' }}
+            />
+            hrs
+          </label>
+
           </div>
-          <button type="submit">Submit</button>
+          <button type="submit" className='btn btn-primary bg-dark text-white'>Submit</button>
         </form>
       </header>
     </div>
